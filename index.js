a76c0c9b691fb594ba1c801742379d37992ddd1a
Leandro Pacheco <leandropacheco02@hotmail.com>
2024-04-30 17:18:38 -0300

    Exercicio feito

--- index.js
+++ index.js
@@ -8,28 +8,32 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Endpoint de login (vulnerável a SQL Injection)
+//1 Endpoint de login (vulnerável a SQL Injection)
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ where: { username, password } });
-  if (user) {
+// a partir daqui não deixamos o password exposto 
+  const user = await User.findOne({ where: { username } });
+  if (user && user.password === password) {
     res.json({ message: 'Login successful', user });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
 });
 
-// Endpoint de listagem de usuários (expondo dados sensíveis)
+//2 Endpoint de listagem de usuários (expondo dados sensíveis)
 app.get('/users', async (req, res) => {
-  const users = await User.findAll({ attributes: ['id', 'username', 'password'] });
+  //Era desnecessário passar o atributo 'password' para a consulta
+  const users = await User.findAll({ attributes: ['id', 'username'] });
   res.json(users);
 });
 
-// Endpoint de detalhe do usuário logado (expondo senha)
+//3 Endpoint de detalhe do usuário logado (expondo senha)
 app.get('/profile', async (req, res) => {
   const { username } = req.query;
   const user = await User.findOne({ where: { username: username ?? null } });
   if (user) {
+    // antes de retornar ao usuário iremos deletar a senha do json retornado
+    delete user.password;
     res.json(user);
   } else {
     res.status(404).json({ message: 'User not found' });
